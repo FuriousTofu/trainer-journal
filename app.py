6f4d0d28bc8dc71a313679916fdb20b03e80b0c0
Maksym Krot <furiousfungus1@gmail.com>
2025-09-22 16:45:49 +0300

    Alembic 1st migration

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 # https://flask.palletsprojects.com/en/stable/patterns/appfactories/
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trainer.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/trainer.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
