5da13a4b065c03b27cc5787106d44a3f10c105d0
Maksym Krot <furiousfungus1@gmail.com>
2025-09-29 16:51:28 +0300

    add layout.html, login.html, register.html +add Flask-WTF do requirements

--- routes.py
+++ routes.py
@@ -1,11 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    return "Привіт з routes.py!"
+    return render_template("main.html")
 
 @bp.route("/clients")
 def clients():
     return "Тут буде список клієнтів"
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
