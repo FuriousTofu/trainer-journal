f9696a53c4f9984b8ecd807d1ca326869b142a18
Maksym Krot <furiousfungus1@gmail.com>
2025-10-05 18:03:13 +0300

    Fixde flash message, added register logick

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ db = SQLAlchemy()
 
 # https://flask.palletsprojects.com/en/stable/patterns/appfactories/
 def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/trainer.db"
+    app = Flask(__name__, instance_relative_config=True)
+    app.config["SECRET_KEY"] = "so_secret_wery_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trainer.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
     from routes import bp as main_bp
     app.register_blueprint(main_bp)
-
+    with app.app_context():
+        print("DB path:", db.engine.url.database)
     return app
--- routes.py
+++ routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError
+from app import db
+from models import Trainer, Client, Session, Exercise, SessionExercise
 
 bp = Blueprint("main", __name__)
 
@@ -16,4 +21,33 @@ def login():
 
 @bp.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        name = (request.form.get("username") or "").strip()
+        email = (request.form.get("email") or "").strip().lower()
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+        if not name or not email or not password or not password2:
+            flash("Please fill in all required fields.", "warning")
+            return render_template("register.html"), 400
+        
+        elif password != password2:
+            flash("Passwords do not match.", "warning")
+            return render_template("register.html"), 400
+        elif len(password) < 8:
+            flash("Password must be at least 8 characters long.", "warning")
+            return render_template("register.html"), 400
+        else:
+            password_hash = generate_password_hash(password)
+            new_trainer = Trainer(name=name, email=email, password_hash=password_hash)
+            db.session.add(new_trainer)
+            try:
+                db.session.commit()
+                flash("Registration successful. Please log in.", "success")
+                login_user(new_trainer)
+                return redirect(url_for(".index"))
+            except IntegrityError:
+                db.session.rollback()
+                flash("Email already registered. Please use a different email.", "error")
+                return render_template("register.html"), 409
+    else:
+        return render_template("register.html")
