690427932473ddd7440b86255b98be4df5bfb689
Maksym Krot <furiousfungus1@gmail.com>
2025-10-08 22:49:32 +0300

    added login_manager

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.login_view = "main.login"
 db = SQLAlchemy()
 
 # https://flask.palletsprojects.com/en/stable/patterns/appfactories/
@@ -10,8 +13,9 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trainer.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+    login_manager.init_app(app)
 
     from routes import bp as main_bp
     app.register_blueprint(main_bp)
-
+    
     return app
