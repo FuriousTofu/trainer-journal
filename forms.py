6803aa85652988ab1cf75ce92494dbc1861e5f7a
Maksym Krot <furiousfungus1@gmail.com>
2025-10-08 22:51:48 +0300

    added login form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms import StringField, EmailField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, Regexp
 
 class RegisterForm(FlaskForm):
@@ -15,6 +15,7 @@ class RegisterForm(FlaskForm):
         'Email', 
         validators=[
             DataRequired(message="Email is required."),
+            Length(max=120, message="Email must be at most 120 characters long."),
             Email(message="Invalid email address.")
         ]
     )
@@ -22,14 +23,35 @@ class RegisterForm(FlaskForm):
         'Password', 
         validators=[
             DataRequired(message="Password is required."),
-            Length(min=8, message="Password must be at least 8 characters long.")
+            Length(min=8, message="Password must be at least 8 characters long."),
+            Length(max=1024, message="Ma man, please stop trying to DoS attack me.")
         ]
     )
     password2 = PasswordField(
         'Confirm Password', 
         validators=[
             DataRequired(message="Please confirm your password."),
+            Length(max=1024, message="Nope, just don't."),
             EqualTo('password', message="Passwords must match.")
         ]
     )
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = EmailField(
+        'Email', 
+        validators=[
+            DataRequired(message="Email is required."),
+            Length(max=120, message="Email must be at most 120 characters long."),
+            Email(message="Invalid email address.")
+        ]
+    )
+    password = PasswordField(
+        'Password', 
+        validators=[
+            DataRequired(message="Password is required."),
+            Length(max=1024, message="That's too long, nope.")
+        ]
+    )
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
