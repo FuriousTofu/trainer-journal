6c8f081742b74f0eccdcdcde6e56c427a26282cb
Maksym Krot <furiousfungus1@gmail.com>
2025-11-03 22:13:24 +0200

    + AddSessionForm and AddSessionExerciseForm

--- forms.py
+++ forms.py
@@ -1,16 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import (
-    StringField,
-    EmailField,
-    PasswordField,
-    SubmitField,
-    BooleanField,
-    TextAreaField,
-    IntegerField,
-    SelectField,
+    StringField, EmailField, PasswordField,
+    SubmitField, BooleanField, TextAreaField,
+    IntegerField, SelectField, DateTimeField,
+    FieldList, FormField,
 )
-from wtforms.validators import DataRequired, Email, Length, EqualTo, Regexp, NumberRange
-
+from wtforms.validators import (
+    DataRequired, Email, Length,
+    EqualTo, Regexp, NumberRange,
+)
 
 class RegisterForm(FlaskForm):
     username = StringField(
@@ -119,3 +117,84 @@ class AddClientForm(FlaskForm):
         ]
     )
     submit = SubmitField("Add Client")
+
+class AddSessionExerciseForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    exercise = SelectField(
+        "Exercise",
+        coerce=int,
+        validators=[
+            DataRequired(message="Exercise is required."),
+            NumberRange(min=1, message="Select an exercise.")
+        ]
+    )
+    sets = IntegerField(
+        "Sets",
+        validators=[
+            DataRequired(message="Number of sets is required."),
+            NumberRange(min=1, message="There must be at least 1 set.")
+        ]
+    )
+    reps = IntegerField(
+        "Repetitions per Set",
+        validators=[
+            DataRequired(message="Number of repetitions is required."),
+            NumberRange(min=1, message="There must be at least 1 repetition.")
+        ]
+    )
+    weight = IntegerField(
+        "Weight (kg)",
+        validators=[
+            DataRequired(message="Weight is required."),
+            NumberRange(min=0, message="Weight cannot be negative.")
+        ]
+    )
+
+class AddSessionForm(FlaskForm):
+    client = SelectField(
+        "Client",
+        coerce=int,
+        validators=[
+            DataRequired(message="Client selection is required."),
+            NumberRange(min=1, message="Please select a client.")
+        ]
+    )
+    start_dt = DateTimeField(
+        "Start Date and Time",
+        format="%Y-%m-%dT%H:%M",
+        validators=[DataRequired(message="Start date and time is required.")]
+    )
+    duration_min = IntegerField(
+        "Duration (minutes)",
+        default=60,
+        validators=[
+            DataRequired(message="Duration is required."),
+            NumberRange(min=1, max=480, message="Duration must be less than 480 minutes.")
+        ]
+    )
+    mode = SelectField(
+        "Session Mode",
+        choices=[('online', 'Online'),
+                 ('offline', 'Offline')
+        ],
+        default='offline',
+        validators=[DataRequired(message="Session mode is required.")]
+    )
+    price = IntegerField(
+        "Session Price",
+        validators=[
+            DataRequired(message="Session price is required."),
+            NumberRange(min=0, message="Price must be non-negative.")
+        ]
+    )
+    notes = TextAreaField(
+        "Notes (Optional)",
+        validators=[
+            Length(max=2048, message="Notes must be at most 2048 characters long.")
+        ]
+    )
+
+    exercises = FieldList(FormField(AddSessionExerciseForm), min_entries=3, max_entries=30)
+    submit = SubmitField("Add Session")
