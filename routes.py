07e8f1a2b7f720b9b0c25527a562dcf9a3f561f2
Maksym Krot <furiousfungus1@gmail.com>
2025-11-03 22:17:40 +0200

    Add Session route

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.exc import IntegrityError
@@ -6,7 +6,10 @@ from sqlalchemy import select
 
 from app import db
 from models import Trainer, Client, Session, Exercise, SessionExercise
-from forms import RegisterForm, LoginForm, AddExerciseForm, AddClientForm
+from forms import (
+    RegisterForm, LoginForm, AddExerciseForm, 
+    AddClientForm, AddSessionForm, AddSessionExerciseForm
+)
 
 bp = Blueprint("main", __name__)
 
@@ -15,10 +18,116 @@ bp = Blueprint("main", __name__)
 def index():
     return render_template("main.html")
 
-@bp.route("/sessions")
+@bp.route("/sessions", methods=["GET", "POST"])
 @login_required
 def sessions():
-    return "Тут буде список сесій"
+    return render_template("sessions.html")
+
+@bp.route("/sessions/add", methods=["GET", "POST"])
+@login_required
+def add_session():
+    form = AddSessionForm()
+
+    form.client.render_kw = {
+        "hx-get": url_for(".client_price"),
+        "hx-target": "#price-wrapper",
+        "hx-trigger": "change",
+        "hx-swap": "outerHTML"
+    } # God bless HTMX
+
+    clients = db.session.execute(
+        select(Client.id, Client.name)
+        .where(Client.trainer_id == current_user.id)
+        .order_by(Client.name)
+    ).all()
+    form.client.choices = [(0, "Select Client")] + [(c.id, c.name) for c in clients]
+
+    exercises = db.session.execute(
+        select(Exercise.id, Exercise.name)
+        .where(Exercise.trainer_id == current_user.id)
+        .order_by(Exercise.name)
+    ).all()
+    exercise_choices = [(0, "Select Exercise")] + [(e.id, e.name) for e in exercises]
+    for subform in form.exercises:
+        subform.exercise.choices = exercise_choices
+    
+    if form.validate_on_submit():
+        client = db.session.get(Client, form.client.data)
+        if not client or client.trainer_id != current_user.id:
+            abort(404)
+        
+        try:
+            s = Session(
+                client_id=form.client.data,
+                start_dt=form.start_dt.data,
+                duration_min=form.duration_min.data,
+                mode=form.mode.data,
+                price=form.price.data,
+                notes=form.notes.data.strip() if form.notes.data else None,
+            )
+            db.session.add(s)
+            db.session.flush() # to use s.id below
+
+            for exercise_form in form.exercises:  
+                sub: AddSessionExerciseForm = exercise_form.form # for code readability
+                
+                ex = db.session.get(Exercise, sub.exercise.data)
+                if not ex or ex.trainer_id != current_user.id:
+                    abort(404)
+
+                se = SessionExercise(
+                    session_id=s.id,
+                    client_id=s.client_id,
+                    exercise_id=sub.exercise.data,
+                    sets=sub.sets.data,
+                    reps=sub.reps.data,
+                    weight=sub.weight.data,
+                )
+                db.session.add(se)
+            db.session.commit()
+            flash("Session added successfully", "success")
+            return redirect(url_for(".sessions"))
+        
+        except IntegrityError:
+            db.session.rollback()
+            flash(
+                (
+                    "Database error occurred."
+                    "Please check your inputs and try again."
+                ), 
+                "danger"
+            )
+        
+        except Exception:
+            db.session.rollback()
+            flash(
+                (
+                    "An unexpected error occured."
+                    "Please contact support if the issue persists."
+                ), 
+                "danger"
+            )
+
+    return render_template("add_session.html", form=form)
+
+@bp.route("/sessions/client-price")
+@login_required
+def client_price():
+    if not request.headers.get("HX-Request"):
+        abort(404)
+    client_id = request.args.get("client", type=int)
+
+    form = AddSessionForm()
+    if not client_id:
+        form.price.data = 0
+        return render_template("helpers/_price_field.html", form=form)
+    
+    client = Client.query.filter_by(
+        id=client_id,
+        trainer_id=current_user.id
+    ).first_or_404()
+    form.price.data = client.price or 0
+    return render_template("helpers/_price_field.html", form=form)
 
 @bp.route("/clients", methods=["GET", "POST"])
 @login_required
