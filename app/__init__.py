34fa59b9fe240e0a73dfbdb729b296cccb504f6c
Maksym Krot <furiousfungus1@gmail.com>
2025-12-22 20:58:28 +0200

    add PRAGMA foreign_keys for dev env

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,36 @@
 from flask import Flask
+from flask_wtf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+from sqlalchemy import event
+from sqlalchemy.engine import Engine 
 
 from .config import DevelopmentConfig
 from .utils import init_template_filters
 
 login_manager = LoginManager()
 login_manager.login_view = "main.login"
-db = SQLAlchemy()
 
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+
+def _enable_sqlite_fk(app: Flask):
+    """
+    Enable foreign key for SQLite databases.
+    Dev environment only, for on cascade deletes to work.
+    """
+    if not app.config.get("ENABLE_SQLITE_FK"):
+        return
+    @event.listens_for(Engine, "connect")
+    def _set_sqlite_pragma(dbapi_connection, connection_record):
+        try:
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON")
+            cursor.close()
+        except Exception:
+            pass
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
@@ -21,7 +43,9 @@ def create_app(config_class=DevelopmentConfig):
 
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
+    _enable_sqlite_fk(app)
     init_template_filters(app)
 
     from app.routes import bp as main_bp
--- app/config.py
+++ app/config.py
@@ -13,3 +13,4 @@ class DevelopmentConfig:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    ENABLE_SQLITE_FK = True
