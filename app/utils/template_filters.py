6169f1d3344522f119ec86912ef625d1ab29cd59
Maksym Krot <furiousfungus1@gmail.com>
2026-01-08 21:25:12 +0200

    timezone filter

--- app/utils/template_filters.py
+++ app/utils/template_filters.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 
 def dt_no_seconds(value, format="%d.%m.%Y %H:%M"):
@@ -6,7 +7,12 @@ def dt_no_seconds(value, format="%d.%m.%Y %H:%M"):
         return ""
     if not isinstance(value, datetime):
         return value
-    return value.strftime(format)
+
+    if value.tzinfo is None:
+        value = value.replace(tzinfo=timezone.utc)
+    local_dt = value.astimezone(ZoneInfo("Europe/Kyiv"))
+
+    return local_dt.strftime(format)
 
 
 def init_template_filters(app):
