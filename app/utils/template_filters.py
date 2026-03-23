8de49d243f17aa679c0ac1436427eb1eec8c532a
Maksym Krot <furiousfungus1@gmail.com>
2026-03-23 14:39:48 +0200

    payment badges now inons

--- app/utils/template_filters.py
+++ app/utils/template_filters.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
 
-def dt_no_seconds(value, format="%d.%m.%Y %H:%M"):
+def dt_no_seconds(value, format="%d.%m %H:%M"):
     if value is None:
         return ""
     if not isinstance(value, datetime):
